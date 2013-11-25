455393858c79bac52750c959b5e0a445e26707eb
badfalcon <badfalcon610@gmail.com>
2013-11-25 09:11:37 +0900

    deleted inport not using

--- src/com/github/badfalcon/SnowBallBattle/SnowListener.java
+++ src/com/github/badfalcon/SnowBallBattle/SnowListener.java
@@ -6,7 +6,6 @@ import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
